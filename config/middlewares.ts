ac7a238fb4657ad96248d9199f4d61923df3cc0e
f64116045 <f64116045@gs.ncku.edu.tw>
2025-06-27 23:11:02 +0800

    feat: connect Strapi to PostgreSQL on Render

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,16 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173', 'http://127.0.0.1:5174', 'https://f64116045.github.io/Personal_Website/'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: '*',
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
